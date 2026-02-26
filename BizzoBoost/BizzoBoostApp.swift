621a9ba361e4f7c93715e3739007b2eddb7eda47
Maksim Kosharny <maksimkosharnyjj@gmail.com>
2026-02-26 16:15:08 +0200

    Initial Commit

---
+++ BizzoBoost/BizzoBoostApp.swift
@@ -0,0 +1,17 @@
+//
+//  BizzoBoostApp.swift
+//  BizzoBoost
+//
+//  Created by Maksim Kosharny on 26.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct BizzoBoostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
